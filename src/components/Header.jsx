b36c2531e699f083d7f198a3a7b0ee2135fd69fd
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-07-16 06:49:34 -0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/logo512.png"
+
+function Header() {
+	return (
+		<header className="app-header">
+			<img src={logo} alt="React logo" />
+			<h1>The React Quiz</h1>
+		</header>
+	)
+}
+
+export default Header
